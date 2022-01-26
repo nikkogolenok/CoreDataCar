c6562d52722d3e5b761782d520804655d9332064
nikita_kogolenok <nikiton17ns@gmail.com>
2022-01-26 14:48:34 +0300

    Initial Commit

---
+++ CoreDataCar/Controller/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  CoreDataCar
+//
+//  Created by Никита Коголенок on 26.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Variables
+    
+    // MARK: - Outlet
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var lastTimeStartedLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var numberOfTripsLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - Actions
+    @IBAction func segmentedAction(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func startEngineAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func rateAction(_ sender: UIButton) {
+    }
+    
+}
+
