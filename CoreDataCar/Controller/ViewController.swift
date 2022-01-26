bb431cae853c3b2bb73f74afc7d7363432d5f226
nikita_kogolenok <nikiton17ns@gmail.com>
2022-01-26 16:56:00 +0300

    Get data from .plist file

--- CoreDataCar/Controller/ViewController.swift
+++ CoreDataCar/Controller/ViewController.swift
@@ -28,6 +28,61 @@ class ViewController: UIViewController {
     }
     
     // MARK: - Methods
+    private func getDataFromFile() {
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        
+        var records = 0
+        
+        do {
+            let count = try context.count(for: fetchRequest)
+            records = count
+            print("Data is there already")
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+        
+        guard records == 0 else { return }
+        
+        // path to file
+        let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist")
+        let dataArray = NSArray(contentsOfFile: pathToFile!)!
+        
+        for dictionary in dataArray {
+            let entity = NSEntityDescription.entity(forEntityName: "Car", in: context)
+            let car = NSManagedObject(entity: entity!, insertInto: context) as! Car
+            
+            let carDictionary = dictionary as! NSDictionary
+            car.mark = carDictionary["mark"] as? String
+            car.model = carDictionary["model"] as? String
+            car.rating = carDictionary["rating"] as! Double
+            car.lastStarted = carDictionary["lastStarted"] as? Date
+            car.timesDriven = carDictionary["timesDriven"] as! Int16
+            car.myChoice = carDictionary["myChoice"] as! Bool
+            
+            if let imageName = carDictionary["imageName"] as? String {
+                let image = UIImage(named: imageName)
+                let imageData = image?.pngData()
+                car.imageData = imageData
+            }
+            
+            if let colorDictionary = carDictionary["tintColor"] as? [String:Float] {
+                car.tintColor = getColor(colorDictionary: colorDictionary)
+            }
+            
+        }
+    }
+    
+    private func getColor(colorDictionary: [String:Float]) -> UIColor {
+        guard let red = colorDictionary["red"],
+              let green = colorDictionary["green"],
+              let blue = colorDictionary["blue"]
+                
+        else {return UIColor() }
+        
+        return UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1.0)
+    }
     
     // MARK: - Actions
     @IBAction func segmentedAction(_ sender: UISegmentedControl) {
