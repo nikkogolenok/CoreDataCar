d395d2d5bef7a4c2ec7009664c9abba1f359e2a9
nikita_kogolenok <nikiton17ns@gmail.com>
2022-01-27 14:15:47 +0300

    Update interface with data

--- CoreDataCar/Controller/ViewController.swift
+++ CoreDataCar/Controller/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
     
     // MARK: - Variables
     var context: NSManagedObjectContext!
+    var car: Car?
+    lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        
+        return dateFormatter
+    }()
     
     // MARK: - Outlet
     @IBOutlet weak var markLabel: UILabel!
@@ -25,35 +33,63 @@ class ViewController: UIViewController {
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        getDataFromFile()
     }
     
     // MARK: - Methods
+    private func updateSegmentedControl() {
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        if let mark = segmentedControl.titleForSegment(at: 0) {
+            fetchRequest.predicate = NSPredicate(format: "mark == %@", mark)
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                car = results[0]
+                guard let selectedCar = car else { return }
+                inserdDataFrom(selectedCar: selectedCar)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func inserdDataFrom(selectedCar car: Car) {
+        self.car = car
+        carImageView.image = UIImage(data: car.imageData!)
+        markLabel.text = car.mark
+        modelLabel.text = car.model
+        ratingLabel.text = "Rating: \(car.rating) / 10"
+        numberOfTripsLabel.text = "Number of Trips \(car.timesDriven)"
+        lastTimeStartedLabel.text = "Last: \(dateFormatter.string(from: car.lastStarted!))"
+        segmentedControl.backgroundColor = car.tintColor as? UIColor
+    }
+    
     private func getDataFromFile() {
+        
         let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
         fetchRequest.predicate = NSPredicate(format: "mark != nil")
         
         var records = 0
         
         do {
-            let count = try context.count(for: fetchRequest)
-            records = count
-            print("Data is there already")
-        }
-        catch {
+            records = try context.count(for: fetchRequest)
+            print("Is Date there already?")
+        } catch let error as NSError {
             print(error.localizedDescription)
         }
         
         guard records == 0 else { return }
         
-        // path to file
-        let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist")
-        let dataArray = NSArray(contentsOfFile: pathToFile!)!
+        guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
+              let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
         
         for dictionary in dataArray {
             let entity = NSEntityDescription.entity(forEntityName: "Car", in: context)
             let car = NSManagedObject(entity: entity!, insertInto: context) as! Car
             
-            let carDictionary = dictionary as! NSDictionary
+            let carDictionary = dictionary as! [String : AnyObject]
             car.mark = carDictionary["mark"] as? String
             car.model = carDictionary["model"] as? String
             car.rating = carDictionary["rating"] as! Double
@@ -61,16 +97,14 @@ class ViewController: UIViewController {
             car.timesDriven = carDictionary["timesDriven"] as! Int16
             car.myChoice = carDictionary["myChoice"] as! Bool
             
-            if let imageName = carDictionary["imageName"] as? String {
-                let image = UIImage(named: imageName)
-                let imageData = image?.pngData()
-                car.imageData = imageData
-            }
+            let imageName = carDictionary["imageName"] as? String
+            let image = UIImage(named: imageName!)
+            let imageData = image!.pngData()
+            car.imageData = imageData
             
-            if let colorDictionary = carDictionary["tintColor"] as? [String:Float] {
+            if let colorDictionary = carDictionary["tintColor"] as? [String : Float] {
                 car.tintColor = getColor(colorDictionary: colorDictionary)
             }
-            
         }
     }
     
